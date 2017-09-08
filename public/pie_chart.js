d9fcf76f87cccca9f9a6bee30a60dca1f3305c22
Matthew Shield <matthewshield@hotmail.co.uk>
2017-09-08 10:50:27 +0100

    Fixed pieChart error

---
+++ public/pie_chart.js
@@ -0,0 +1,32 @@
+var PieChart = function(countries) {
+ var container = document.querySelector("#pie-chart");
+ console.log(countries);
+ var chart = new Highcharts.Chart({
+
+  chart:  {
+   type:  'pie',
+   renderTo:  container
+ },
+
+ title:  {
+  text: "Country Populations"
+},
+
+credits:  {
+  enabled:  false
+},
+
+
+series:  [
+      {
+        name: "Population",
+        data: 
+          countries
+        
+      }
+    ]
+
+
+
+})
+}
